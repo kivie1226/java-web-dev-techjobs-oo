38abca6156d2e8c0a866da88194781541079aac6
Kimberly Ivie <ktbt02@yahoo.com>
2019-12-01 13:18:03 -0600

    finished the third test

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -3,14 +3,15 @@ package org.launchcode.techjobs_oo.Tests;
 import org.junit.Test;
 import org.launchcode.techjobs_oo.*;
 
-import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.*;;
 
 
 public class JobTest {
 
         Job testJob1 = new Job();
         Job testJob2 = new Job();
-        Job testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob3 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        Job testJob4 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
 
         //Making sure different Id's are assigned:
         @Test
@@ -21,7 +22,15 @@ public class JobTest {
         //Making sure Job contains all 6 fields:
     @Test
     public void testJobConstructorSetsAllFields() {
-
+        assertEquals(3, testJob3.getId());
+        assertEquals("Product Tester", testJob3.getName());
+        assertEquals("ACME", testJob3.getEmployer().toString());
+        assertEquals("Desert", testJob3.getLocation().toString());
+       // assertEquals("Quality Control", testJob3.getPositionType().toString());
+        assertEquals("Persistence", testJob3.getCoreCompetency().toString());
     }
-
+    @Test
+    public void testJobsForEquality() {
+            assertFalse(testJob3.equals(testJob4));
+    }
 }
